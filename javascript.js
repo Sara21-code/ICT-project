4d57910ebfa2c45656c102df0b91222dd8c3ff49
Sara Akram123 <frausara001@gmail.com>
2026-06-10 17:22:12 +0500

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,17 @@
+
+console.log("this is basic java script");
+let age="24";
+age="25"
+console.log(age)
+const Student={
+    fullname:"saraakram",
+    rollno:"baif25m054",
+    discipline:"BSAI",
+}
+console.log(Student)
+console.log(Student["rollno"])
+
+console.log("let ki value update ho skte but const ki value cinstant rhtee hai")
+console.log("bus itna hi ata hai mujhay")
+price="10"
+console.log(price)
